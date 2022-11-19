f9ce1292492254ebab02692180ac343ac4a071a6
András Rajkai <rajkaiandras@gmail.com>
2022-11-19 20:47:45 +0100

    added SignUp

--- src/pages/signUp/SignUp.jsx
+++ src/pages/signUp/SignUp.jsx
@@ -1,8 +1,51 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 // styles
 import './SignUp.css';
 
 export const SignUp = () => {
-  return <div className="SignUp">Sign Up</div>;
+  const [displayName, setDisplayName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(displayName, email, password);
+  };
+
+  return (
+    <section className="SignUp">
+      <form onSubmit={handleSubmit} className="sign-up-form">
+        <h2 className="sign-up-title">Sign Up</h2>
+        <label>
+          <span>Display name</span>
+          <input
+            type="text"
+            onChange={(e) => setDisplayName(e.target.value)}
+            value={displayName}
+            placeholder="Display name..."
+          />
+        </label>
+        <label>
+          <span>E-mail</span>
+          <input
+            type="email"
+            onChange={(e) => setEmail(e.target.value)}
+            value={email}
+            placeholder="Your e-mail..."
+          />
+        </label>
+        <label>
+          <span>Password</span>
+          <input
+            type="password"
+            onChange={(e) => setPassword(e.target.value)}
+            value={password}
+            placeholder="Your password..."
+          />
+        </label>
+        <button className="sign-up-btn">Sign Up</button>
+      </form>
+    </section>
+  );
 };
