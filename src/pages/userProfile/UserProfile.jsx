8185f3c8a3c3e5ea07a6f840a0e9f1bfd39b54af
András Rajkai <rajkaiandras@gmail.com>
2022-11-21 10:28:00 +0100

    added UserProfile styling

--- src/pages/userProfile/UserProfile.jsx
+++ src/pages/userProfile/UserProfile.jsx
@@ -1,8 +1,27 @@
 import React from 'react';
 
+// hooks
+import { useAuthContext } from '../../hooks/useAuthContext';
+
 // styles
 import './userProfile.css';
 
 export const UserProfile = () => {
-  return <div className="UserProfile">User Profile</div>;
+  const { user } = useAuthContext();
+
+  return (
+    <div className="UserProfile">
+      <div className="profile-container">
+        <h1>Profile Details</h1>
+        <p>Display name:</p>
+        <p>{user.displayName}</p>
+        <p>Email address:</p>
+        <p>{user.email}</p>
+        <p>Profile creation:</p>
+        <p>{user.metadata.creationTime}</p>
+        <p>Last sign in:</p>
+        <p>{user.metadata.lastSignInTime}</p>
+      </div>
+    </div>
+  );
 };
