b198cc6be0dd99d95a655eb5ace3f5a1c0c78f67
András Rajkai <rajkaiandras@gmail.com>
2022-11-26 18:06:06 +0100

    added footer links

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,8 +1,19 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 // styles
 import './Footer.css';
 
 export const Footer = () => {
-  return <footer className="Footer">Footer</footer>;
+  return (
+    <footer className="Footer">
+      <div className="developer-container">
+        developed by <span className="developer-name">András Rajkai</span>
+      </div>
+      <div className="footer-links-container">
+        <Link to="/aboutme">about me</Link>
+        <Link to="/contacts">contacts</Link>
+      </div>
+    </footer>
+  );
 };
