7c1370b9e5b7e60f18e4ac939bb47cb0627d3840
András Rajkai <rajkaiandras@gmail.com>
2022-11-27 11:24:26 +0100

    intro animation finished

--- src/Marvel.js
+++ src/Marvel.js
@@ -35,7 +35,7 @@ export const Marvel = () => {
   useEffect(() => {
     setTimeout(() => {
       setIsIntro(false);
-    }, 8000);
+    }, 6000);
   }, []);
 
   // subscription visibility
--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -7,7 +7,19 @@ export const Intro = () => {
   return (
     <div className="Intro">
       <div className="header-cover"></div>
-      <div className="content-cover"></div>
+      <div className="content-cover">
+        <div className="title-container">
+          <h1 className="hero-title">Heroes live among us</h1>
+          <div className="hero-title-cover"></div>
+        </div>
+        <div className="subtitle-container">
+          <h1 className="hero-subtitle">
+            Discover the <span className="power-highlight">POWER</span> of comic
+            books
+          </h1>
+          <div className="hero-subtitle-cover"></div>
+        </div>
+      </div>
       <div className="footer-cover"></div>
     </div>
   );
