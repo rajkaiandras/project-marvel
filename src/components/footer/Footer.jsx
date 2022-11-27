1396c41416803719355dd7c3c16fe6c5f6cf2c4c
András Rajkai <rajkaiandras@gmail.com>
2022-11-27 20:57:18 +0100

    added subscription to footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,10 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
+
+// components
+import { Subscription } from '../subscription/Subscription';
 
 // styles
 import './Footer.css';
 
 export const Footer = () => {
+  const [subscriptionVisibility, setSubscriptionVisibility] = useState(false);
+
+  const closeSubscription = () => {
+    setSubscriptionVisibility(false);
+  };
+
   return (
     <footer className="Footer">
       <div className="developer-container">
@@ -16,6 +25,16 @@ export const Footer = () => {
       <div className="footer-links-container">
         <Link to="/aboutme">about me</Link>
         <Link to="/contacts">contacts</Link>
+        <p
+          onClick={() => {
+            setSubscriptionVisibility(!subscriptionVisibility);
+          }}
+        >
+          subscription
+        </p>
+        {subscriptionVisibility && (
+          <Subscription closeSubscription={closeSubscription} />
+        )}
       </div>
     </footer>
   );
