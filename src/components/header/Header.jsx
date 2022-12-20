c3ef8aaeae857f9944eb6e19426ef1baeeaacb77
András Rajkai <rajkaiandras@gmail.com>
2022-12-20 18:55:25 +0100

    fixed mobile navigation bar bug

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -2,8 +2,8 @@ import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 
 // hooks
-import { useLogOut } from '../../hooks/useLogOut';
 import { useAuthContext } from '../../hooks/useAuthContext';
+import { useLogOut } from '../../hooks/useLogOut';
 
 // components
 import { MarvelLogo } from '../../components/marvelLogo/MarvelLogo';
@@ -44,26 +44,51 @@ export const Header = () => {
       </button>
 
       <nav className={navBarVisibility ? 'nav-bar active' : 'nav-bar'}>
-        <Link to="/">Home</Link>
-        <Link to="/comics">Comics</Link>
-        <Link to="/characters">Characters</Link>
+        <Link to="/" onClick={toggleNavBar}>
+          Home
+        </Link>
+        <Link to="/comics" onClick={toggleNavBar}>
+          Comics
+        </Link>
+        <Link to="/characters" onClick={toggleNavBar}>
+          Characters
+        </Link>
 
         {!user && (
           <>
-            <Link to="/login">Log In</Link>
-            <Link to="/signup">Sign Up</Link>
+            <Link to="/login" onClick={toggleNavBar}>
+              Log In
+            </Link>
+            <Link to="/signup" onClick={toggleNavBar}>
+              Sign Up
+            </Link>
           </>
         )}
 
         {user && (
           <>
-            <Link className="favorites-btn" to={'/favorites'}>
+            <Link
+              className="favorites-btn"
+              to={'/favorites'}
+              onClick={toggleNavBar}
+            >
               Favorites
             </Link>
-            <Link className="user-profile-btn" to={'/userprofile'}>
+            <Link
+              className="user-profile-btn"
+              to={'/userprofile'}
+              onClick={toggleNavBar}
+            >
               {user.displayName}
             </Link>
-            <Link className="log-out-btn" to="#" onClick={logOut}>
+            <Link
+              className="log-out-btn"
+              to="#"
+              onClick={() => {
+                toggleNavBar();
+                logOut();
+              }}
+            >
               Log Out
             </Link>
           </>
